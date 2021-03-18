0112a2542a06e9990ff8a29763746e0502d1b3af
cmrowles89 <christina@snapit.solutions>
2021-03-18 16:11:07 -0500

    use span tags for likes and dislikes

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-//I pulled the thumbs up and thumbs down from DOM using their class and created a variable
-let thumbsup = document.querySelector('.thumbsup')
-let thumbsdown = document.querySelector('.thumbsdown')
-//let span = document.querySelector('span')
-let likesBtn = document.querySelector('.likes')
-let dislikesBtn = document.querySelector('.dislikes')
-//let span = document.getElementByTagName('span')
 
-thumbsup.addEventListener('click', function (){
-//console.log('test') -This is to check if my click function is working
-likesBtn.value = parseInt(likesBtn.value) + 1;
+const thumbsupBtn = document.querySelector('.thumbsup')
+const thumbsdownBtn = document.querySelector('.thumbsdown')
+const likesBtn = document.querySelector('.likes')
+const dislikesBtn = document.querySelector('.dislikes')
+
+
+thumbsupBtn.addEventListener('click',() =>{
+likesBtn.innerText = parseInt(likesBtn.innerText) + 1;
 });
-//alert('This test was successful') - This is to test if the alert was working
 
-thumbsdown.addEventListener('click', () =>{
-//console.log('test')
-dislikesBtn.value = parseInt(dislikesBtn.value) + 1;
+thumbsdownBtn.addEventListener('click', () =>{
+dislikesBtn.innerText = parseInt(dislikesBtn.innerText) + 1;
 });
